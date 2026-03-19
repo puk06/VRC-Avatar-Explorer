80a0d07580b2040011e0d0c4a33af16bc37ee990
Puk06 <86549420+puk06@users.noreply.github.com>
2026-03-19 12:56:42 +0900

    refactor: improve author aggregation by using distinct and ordering authors

--- AvatarExplorer.Core/Services/Items/ItemAuthorAggregator.cs
+++ AvatarExplorer.Core/Services/Items/ItemAuthorAggregator.cs
@@ -8,8 +8,9 @@ internal static class ItemAuthorAggregator
     internal static ImmutableArray<ItemCountInfo> Aggregate(IEnumerable<Item> items)
     {
         return items
-            .GroupBy(i => new { i.Author, i.AuthorThumbnmailFileName })
-            .Select(i => new ItemCountInfo(new Author { Name = i.Key.Author, AuthorThumbnailFileName = i.Key.AuthorThumbnmailFileName }, i.Count()))
+            .DistinctBy(i => i.Author)
+            .OrderBy(i => i.Author)
+            .Select(i => new ItemCountInfo(new Author { Name = i.Author, AuthorThumbnailFileName = i.AuthorThumbnmailFileName }, items.Count(item => item.Author == i.Author)))
             .ToImmutableArray();
     }
 }
