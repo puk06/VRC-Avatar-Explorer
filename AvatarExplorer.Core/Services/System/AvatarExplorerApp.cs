283ed68fc23b8536a134299ccaa695013c19b229
Puk06 <86549420+puk06@users.noreply.github.com>
2026-03-24 18:41:45 +0900

    chore: bump version 2.0.0-beta.15

--- AvatarExplorer.Core/Services/System/AvatarExplorerApp.cs
+++ AvatarExplorer.Core/Services/System/AvatarExplorerApp.cs
@@ -18,7 +18,7 @@ namespace AvatarExplorer.Core.Services.System;
 
 public class AvatarExplorerApp
 {
-    public static readonly string CurrentVersion = "2.0.0-beta.14";
+    public static readonly string CurrentVersion = "2.0.0-beta.15";
 
     public static AvatarExplorerApp Instance { get; private set; } = new AvatarExplorerApp();
 
