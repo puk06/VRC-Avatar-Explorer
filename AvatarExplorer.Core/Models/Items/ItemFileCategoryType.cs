e9ecb8acb2dffcfa9358d422995fd6cee7c920a2
Puk06 <86549420+puk06@users.noreply.github.com>
2026-04-10 05:01:01 +0900

    feat: reorder item file category types

--- AvatarExplorer.Core/Models/Items/ItemFileCategoryType.cs
+++ AvatarExplorer.Core/Models/Items/ItemFileCategoryType.cs
@@ -6,18 +6,6 @@ namespace AvatarExplorer.Core.Models.Items;
 public enum ItemFileCategoryType
 {
     None,
-    
-    [ExtensionsFilter(".psd|.psb|.clip|.kra|.sai|.sai2|.blend|.fbx|.obj|.gltf|.glb|.dae|.stl")]
-    [LocalizationKey(LocalizationKey.FileCategory.Modification)]
-    Modification,
-
-    [ExtensionsFilter(".png|.jpg|.jpeg|.webp|.tga|.bmp|.dds|.tiff|.tif|.gif|.hdr|.exr")]
-    [LocalizationKey(LocalizationKey.FileCategory.Texture)]
-    Texture,
-
-    [ExtensionsFilter(".txt|.md|.pdf|.rtf|.doc|.docx")]
-    [LocalizationKey(LocalizationKey.FileCategory.Document)]
-    Document,
 
     [ExtensionsFilter(".unitypackage")]
     [LocalizationKey(LocalizationKey.FileCategory.Unitypackage)]
@@ -28,13 +16,25 @@ public enum ItemFileCategoryType
     [LocalizationKey(LocalizationKey.FileCategory.Material)]
     Material,
 
-    [ExtensionsFilter(".url")]
-    [LocalizationKey(LocalizationKey.FileCategory.UrlShortcut)]
-    UrlShortcut,
+    [ExtensionsFilter(".png|.jpg|.jpeg|.webp|.tga|.bmp|.dds|.tiff|.tif|.gif|.hdr|.exr")]
+    [LocalizationKey(LocalizationKey.FileCategory.Texture)]
+    Texture,
+
+    [ExtensionsFilter(".psd|.psb|.clip|.kra|.sai|.sai2|.blend|.fbx|.obj|.gltf|.glb|.dae|.stl")]
+    [LocalizationKey(LocalizationKey.FileCategory.Modification)]
+    Modification,
+
+    [ExtensionsFilter(".txt|.md|.pdf|.rtf|.doc|.docx")]
+    [LocalizationKey(LocalizationKey.FileCategory.Document)]
+    Document,
 
     [ExtensionsFilter(".ttf|.otf|.woff|.woff2|.eot|.fon")]
     [LocalizationKey(LocalizationKey.FileCategory.Font)]
     Font,
+
+    [ExtensionsFilter(".url")]
+    [LocalizationKey(LocalizationKey.FileCategory.UrlShortcut)]
+    UrlShortcut,
 
     [LocalizationKey(LocalizationKey.FileCategory.Unknown)]
     Unknown
