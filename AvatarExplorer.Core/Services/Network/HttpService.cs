059f7dc185d7a0a13b09825f3cb7ed6971577fb9
puk06 <86549420+puk06@users.noreply.github.com>
2026-08-07 03:19:38 +0900

    chore(network): add User-Agent header to HTTP requests

--- AvatarExplorer.Core/Services/Network/HttpService.cs
+++ AvatarExplorer.Core/Services/Network/HttpService.cs
@@ -1,6 +1,14 @@
+using AvatarExplorer.Core.Services.System;
+
 namespace AvatarExplorer.Core.Services.Network;
 
 public static class HttpService
 {
-    public static readonly HttpClient Client = new();
+    public static readonly HttpClient Client = new()
+    {
+        DefaultRequestHeaders =
+        {
+            { "User-Agent", $"VRC-Avatar-Explorer/{AvatarExplorerApp.CurrentVersion}" }
+        }
+    };
 }
