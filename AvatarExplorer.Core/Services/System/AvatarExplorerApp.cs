052e2350dc299e190b5465edf41f14bc3c07dff7
puk06 <86549420+puk06@users.noreply.github.com>
2026-08-13 07:44:14 +0900

    chore: bump version to 2.7.2

--- AvatarExplorer.Core/Services/System/AvatarExplorerApp.cs
+++ AvatarExplorer.Core/Services/System/AvatarExplorerApp.cs
@@ -9,7 +9,7 @@ namespace AvatarExplorer.Core.Services.System;
 
 public class AvatarExplorerApp
 {
-    public static readonly string CurrentVersion = "2.7.1";
+    public static readonly string CurrentVersion = "2.7.2";
 
     private static readonly AvatarExplorerApp _instance = new();
     public static AvatarExplorerApp Instance => _instance;
