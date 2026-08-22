1a2e4dff5c38a3cfd61f977e53a919e346a133cb
ぷこるふ <86549420+puk06@users.noreply.github.com>
2026-08-22 14:23:55 +0900

    chore: bump version to 2.8.0-beta.7

--- AvatarExplorer.Core/Services/System/AvatarExplorerApp.cs
+++ AvatarExplorer.Core/Services/System/AvatarExplorerApp.cs
@@ -9,7 +9,7 @@ namespace AvatarExplorer.Core.Services.System;
 
 public sealed class AvatarExplorerApp
 {
-    public static readonly string CurrentVersion = "2.8.0-beta.6";
+    public static readonly string CurrentVersion = "2.8.0-beta.7";
 
     public static AvatarExplorerApp Instance { get; } = new();
 
