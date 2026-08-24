17ba4ef6fca986583d67c95e890dbe6b7b05cee7
ぷこるふ <86549420+puk06@users.noreply.github.com>
2026-08-24 14:02:14 +0900

    feat(settings): add report issue button to about tab

--- AvatarExplorer.Core/Data/Links/SoftwareLink.cs
+++ AvatarExplorer.Core/Data/Links/SoftwareLink.cs
@@ -4,6 +4,7 @@ public static class SoftwareLink
 {
     public const string RepositoryURL = "https://github.com/puk06/VRC-Avatar-Explorer";
     public const string UpdateCheckURL = "https://update.pukosrv.net/check/avatarexplorerv2";
-    public const string LatestReleasePageURL = "https://github.com/puk06/VRC-Avatar-Explorer/releases/latest";
-    public const string ReleasePageURL = "https://github.com/puk06/VRC-Avatar-Explorer/releases/tag/v{0}";
+    public const string LatestReleasePageURL = RepositoryURL + "/releases/latest";
+    public const string IssuesURL = RepositoryURL + "/issues";
+    public const string ReleasePageURL = RepositoryURL + "/releases/tag/v{0}";
 }
