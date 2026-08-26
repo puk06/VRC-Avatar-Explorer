e5068aa589848e7e5b84aae0f800d53e5e7d74ef
ぷこるふ <86549420+puk06@users.noreply.github.com>
2026-08-26 09:02:24 +0900

    fix(ui): apply anti-aliasing setting to avatar thumbnails in select/resolve overlays (#768)

--- AvatarExplorer.UI/Views/Overlays/ResolveTempAvatarOverlay.axaml.cs
+++ AvatarExplorer.UI/Views/Overlays/ResolveTempAvatarOverlay.axaml.cs
@@ -1,4 +1,8 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using AvatarExplorer.UI.Extensions;
 using AvatarExplorer.UI.Services;
 
 namespace AvatarExplorer.UI.Views.Overlays;
@@ -9,5 +13,14 @@ public partial class ResolveTempAvatarOverlay : UserControl
     {
         InitializeComponent();
         DataContext = InstanceRepository.MainWindow.ResolveTempAvatarVM;
+    }
+
+    private void OnItemImageLoaded(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Image image) return;
+
+        var bitmapInterpolationMode = InstanceRepository.UserPreferences.AntiAliasingMode.GetInterpolationMode();
+        if (bitmapInterpolationMode != BitmapInterpolationMode.None && bitmapInterpolationMode != BitmapInterpolationMode.Unspecified)
+            RenderOptions.SetBitmapInterpolationMode(image, bitmapInterpolationMode);
     }
 }
